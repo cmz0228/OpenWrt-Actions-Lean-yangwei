1c182bf9af4b20ce700b50508afc479fb66379ff
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-02-09 21:22:14 +0800

    Update OpenWrt-x86-64.sh

--- OpenWrt-x86-64.sh
+++ OpenWrt-x86-64.sh
@@ -12,9 +12,9 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 替换默认Argon主题
 rm -rf package/lean/luci-theme-argon
-#git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-#git cloen https://github.com/Aslin-Ameng/luci-theme-Light.git package/luci-theme-Light
-#git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git cloen https://github.com/Aslin-Ameng/luci-theme-Light.git package/luci-theme-Light
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 
 # 添加第三方软件包
@@ -25,6 +25,7 @@ git clone https://github.com/Lienol/openwrt-package.git package/openwrt-package
 #git clone https://github.com/Flowers-in-thorns/my.git package/Flowers-in-thorns
 #git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo package/luci-app-ssr-plus-Jo
 git clone https://github.com/Flowers-in-thorns/luci-app-vssr-coexist.git package/luci-app-vssr-coexist
+git clone https://github.com/pymumu/smartdns.git package/luci-app-smartdns
 
 # uci-app-diskman
 git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
@@ -163,6 +164,9 @@ CONFIG_PACKAGE_luci-app-vssr-coexist=y
 CONFIG_PACKAGE_luci-app-docker=y
 CONFIG_PACKAGE_luci-app-dockerman=y
 CONFIG_PACKAGE_luci-app-diskman=y
+CONFIG_PACKAGE_luci-app-aria2=y
+CONFIG_PACKAGE_luci-app-samba4=y
+CONFIG_PACKAGE_luci-app-smartdns=y
 CONFIG_LUCI_LANG_zh-tw=y
 CONFIG_LUCI_LANG_en=y
 EOF
@@ -186,9 +190,9 @@ EOF
 
 # LuCI主题:
 cat >> .config <<EOF
-#CONFIG_PACKAGE_luci-theme-argon=y
-#CONFIG_PACKAGE_luci-theme-Light=y
-#CONFIG_PACKAGE_luci-theme-atmaterial=y
+CONFIG_PACKAGE_luci-theme-argon=y
+CONFIG_PACKAGE_luci-theme-Light=y
+CONFIG_PACKAGE_luci-theme-atmaterial=y
 CONFIG_PACKAGE_luci-theme-opentomcat=y
 EOF
 
